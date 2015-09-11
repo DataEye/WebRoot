9bdd0b33417658efad1828a0ff973c07986f2e8e
simongfxu <xugaofan@gmail.com>
2015-09-11 16:24:10 +0800

    todo

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
-// gulp-racpack
 var gulp = require('gulp')
 var trac = require('gulp-trac')
 var rename = require('gulp-rename')
+var optimize = require('gulp-requirejs-optimize')
 
 gulp.task('transformSingleFileComponent', function() {
   return gulp.src('assets/components/*.html')
@@ -10,4 +10,11 @@ gulp.task('transformSingleFileComponent', function() {
     .pipe(gulp.dest('assets/js/components/'))
 })
 
-gulp.task('default', ['transformSingleFileComponent'])
+gulp.task('amdOptimize', ['transformSingleFileComponent'], function() {
+  return gulp.src("assets/js/*.js")
+    // Traces all modules and outputs them in the correct order.
+    .pipe(amdOptimize("main"))
+    .pipe(gulp.dest("assets-build/js"))
+})
+
+gulp.task('default', ['amdOptimize'])
