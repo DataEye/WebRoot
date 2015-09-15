214a22fd53ae10383cc986e9929000b34c108758
simongfxu <xugaofan@gmail.com>
2015-09-15 09:51:06 +0800

    UPDATE DROPDOWN SAMPLE

--- assets/js/app/sample.js
+++ assets/js/app/sample.js
@@ -19,9 +19,10 @@ define([
         template: tpl,
         el: '#wrapper',
         data: {
+          val: 20,
           items: [
             {label: 10, value: 10},
-            {label: 20, value: 20, selected: true},
+            {label: 20, value: 20},
             {label: 50, value: 50}
           ]
         }
@@ -29,7 +30,24 @@ define([
 
       // dropdown内部事件触发
       app.on('selectChange', function(e, value) {
-        console.log(value)
+        console.log('ajaxing with value', value)
+      })
+
+      app.on('changeValue', function(e, val) {
+        app.set('val', val)
+      })
+
+      app.on('changeItems', function() {
+        app.set('items', [
+          {label: 1, value: 1},
+          {label: 2, value: 2},
+          {label: 3, value: 3}
+        ])
+
+        app.set('val', 3)
+
+        var dropdown = app.findComponent('dropdown')
+        console.log('selected', dropdown.get('selected'))
       })
 
       app.fire('selectChange', null, 20)
