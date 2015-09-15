f43605ee13b290bb5dd228a03ea4cd84cfb5453c
simongfxu <xugaofan@gmail.com>
2015-09-15 11:45:57 +0800

    rvc gulpfile updated

---
+++ assets/js/components/dropdown.js
@@ -0,0 +1,124 @@
+define([
+	"jquery",
+	"ractive",
+	"lodash",
+	"require",
+	"ractive"
+], function(
+	__import0__,
+	__import1__,
+	__import2__,
+	require,
+	Ractive
+){
+
+var __options__ = {
+	template: {v:3,t:[{p:[1,1,0],t:7,e:"div",a:{"class":["btn-group dropdown dropdown-select ",{t:2,x:{r:["isOpen"],s:"_0?\"open\":\"\""},p:[1,48,47]}],style:"display: inline-block;"},f:[{p:[3,3,112],t:7,e:"button",a:{"class":"btn btn-select dropdown-toggle",type:"button"},v:{click:{n:[{t:2,x:{r:["items.length"],s:"_0?\"toggleShow\":\"\""},p:[4,21,193]}],d:[]}},f:[{t:4,f:[{t:4,f:[{t:4,f:[{p:[8,11,309],t:7,e:"i",a:{"class":["fa ",{t:2,r:"icon",p:[8,24,322]}]}}],r:"icon",p:[7,9,289]}," ",{t:2,r:"label",p:[11,9,364]}],n:50,x:{r:[".value","selected"],s:"_0===_1"},p:[6,7,253]}],r:"items",p:[5,5,236]},{t:4,n:51,f:[{t:2,x:{r:["emptyText"],s:"_0||\"暂无数据\""},p:[14,7,407]}],r:"items"}," ",{p:[17,5,451],t:7,e:"span",a:{"class":"caret"}}]}," ",{t:4,f:[{p:[21,3,508],t:7,e:"ul",a:{"class":"dropdown-menu",style:"display: block;min-width: inherit;max-height: 200px; overflow: auto;"},f:[{t:4,f:[{p:[23,5,633],t:7,e:"li",a:{"class":[{t:2,x:{r:[".value","selected"],s:"_0===_1?\"selected-dropdown-menu\":\"\""},p:[23,16,644]}]},v:{click:{n:"select",d:[{t:2,r:"i",p:[23,90,718]}]}},f:[{p:[24,7,732],t:7,e:"a",a:{tabindex:"-1",href:"javascript:;"},f:[{t:4,f:[{p:[26,9,796],t:7,e:"i",a:{"class":["fa ",{t:2,r:"icon",p:[26,22,809]}]}}],r:"icon",p:[25,9,778]}," ",{t:2,r:"label",p:[28,9,850]}]}]}],i:"i",r:"items",p:[22,5,616]},{t:4,n:51,f:[{p:[32,5,898],t:7,e:"li",a:{"class":""},f:[{p:[33,7,918],t:7,e:"a",a:{href:"javascript:;"},f:[{t:2,x:{r:["emptyText"],s:"_0||\"暂无数据\""},p:[34,9,950]}]}]}],r:"items"}]}],r:"isOpen",p:[20,3,494]}]}]},
+},
+component={},
+__prop__,
+__export__;
+
+  /**
+   * 使用方式
+   * <dropdown items="{{items}}" selected="{{value}}" on-itemchange="selectChange" />
+   * list = [{label: 'X', value: '1'}]
+   */
+  var $ = require('jquery')
+  var Ractive = require('ractive')
+  var _ = require('lodash')
+  var MSG = {
+    SET_VALUE_MANUALLY: '变更数据源后请主动更新value',
+    EMPTY_DATA: '数据源items为空',
+    INVALID_VALUE: '当前设置value未找到'
+  }
+
+  /**
+   * 调整下拉框的位置，避免在底部被遮挡
+   */
+  function adjustPosition(node, margin) {
+    var offset = node.offset()
+    var height = node.outerHeight()
+    var totalHeight = document.documentElement.offsetHeight
+
+    if (offset.top + height > totalHeight - margin) {
+      node.css('top', -1 * height + 'px')
+    }
+  }
+
+  component.exports = {
+    isolated: true,
+    data: {
+      isOpen: false
+    },
+    onrender: function () {
+      var self = this
+
+      /**
+       * 监听数据源改变，其实只是做了数据监测
+       * 检测value是否设置正确即可（需要调研方主动设置）
+       */
+      function onDataSourceChange(items) {
+        if (Array.isArray(items) && items.length) {
+          var value = self.get('selected')
+          var index = _.findIndex(items, {
+            value: value
+          })
+
+          if (index === -1) {
+            console.warn(MSG.SET_VALUE_MANUALLY)
+          }
+        } else {
+          console.warn(MSG.EMPTY_DATA)
+        }
+      }
+
+      function onSelectedChange(value) {
+        var context = _.find(self.get('items'), {value: value})
+        if (!context) {
+          console.warn(MSG.INVALID_VALUE)
+          return
+        }
+
+        // 兼容老事件
+        self.fire('changed', null, context.value, context)
+        // 新的事件
+        self.fire('itemchange', null, context.value, context)
+      }
+
+      self.on('toggleShow', function() {
+        self.toggle('isOpen')
+				var ul = $(self.find('ul'))
+        // 底部footer占位
+        if (ul.length) {
+          adjustPosition(ul, self.get('offset') || 100)
+        }
+      })
+
+      self.on('select', function(e) {
+        self.set('isOpen', false)
+
+        // 不要随便变更selected，如果修改为一个不一致的值会切断外部组件的双向绑定关系
+        self.set('selected', e.context.value)
+      })
+
+      self.observe('items', onDataSourceChange)
+
+      self.observe('selected', onSelectedChange)
+    }
+  }
+
+
+if ( typeof component.exports === "object" ) {
+	for ( __prop__ in component.exports ) {
+		if ( component.exports.hasOwnProperty(__prop__) ) {
+			__options__[__prop__] = component.exports[__prop__];
+		}
+	}
+}
+
+__export__ = Ractive.extend( __options__ );
+
+
+	return __export__;
+});
